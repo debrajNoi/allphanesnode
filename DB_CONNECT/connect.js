6adc4d65791fc65fbb9512dd6c2118db5e37af53
debrajNoi <101630180+debrajNoi@users.noreply.github.com>
2022-03-23 14:58:02 +0530

    server

--- DB_CONNECT/connect.js
+++ DB_CONNECT/connect.js
@@ -3,7 +3,7 @@ const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/Allphanesdatabas
 mongoose.connect(uri,{
     useNewUrlParser: true,
     // useFindAndModify: true,
-    useFindAndModify: false,
+    // useFindAndModify: false,
     useUnifiedTopology: true
 }).then(()=>{
     console.log("Database connection successfull");
