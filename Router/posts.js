49245cc3e810fd23838bf7d5af30295824352ab1
debrajNoi <101630180+debrajNoi@users.noreply.github.com>
2022-04-19 21:15:18 +0530

    server

--- Router/posts.js
+++ Router/posts.js
@@ -7,8 +7,8 @@ const router = express.Router()
 
 const MongoClient = require('mongodb').MongoClient
 // const url = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/Allphanesdatabase' 
-const url = process.env.MONGO_URI 
 
+const url ='mongodb+srv://allphanes:7031445611@allphanescluster.x5i5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const cloudinary=require("cloudinary").v2 
 
 cloudinary.config({ 
