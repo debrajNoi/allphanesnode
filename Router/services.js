9153f2490ab2742c2aa27195ee83630eb46fd154
debrajNoi <101630180+debrajNoi@users.noreply.github.com>
2022-04-21 22:02:45 +0530

    server

--- Router/services.js
+++ Router/services.js
@@ -5,8 +5,8 @@ const postsModel=require("../Model/posts");
 const { ObjectId } = require("mongodb");
 const router = express.Router()
 const MongoClient = require('mongodb').MongoClient
-const url =  'mongodb://127.0.0.1:27017/myFirstDatabase'
-// const url = 'mongodb+srv://allphanes:7031445611@allphanescluster.x5i5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const url =  'mongodb://127.0.0.1:27017/myFirstDatabase'
+const url = 'mongodb+srv://allphanes:7031445611@allphanescluster.x5i5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 router.get("/",async(req,res)=>{
     try{
